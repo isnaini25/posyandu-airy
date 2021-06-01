d27fef0d264224a491d57db6647a84cf53c3cd2e
isnaini25 <75622170+isnaini25@users.noreply.github.com>
2021-06-01 18:35:51 +0700

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,35 +1,26 @@
 <?php 
-// untuk memasukkan file koneksi.php
 include "../lib/koneksi.php";
-// menangkap variable POST dari Form Login/index.php
 $username = $_POST['username'];
 $pass = $_POST['password'];
-// pastikan username dan password adalah huruf dan angka.
-if (!ctype_alnum($username) OR !ctype_alnum($pass)) {
-	echo "<center>Login Gagal !!! <br>
-		Username atau Password Anda tidak benar.<br>
-		Atau account anda sedang diblokir.<br>
-		</center>";
-	echo "<a href=index.php><b>Ulangi Lagi</b></a>";
-}else{
-	$login = mysqli_query($koneksi, "SELECT * FROM tbl_kader WHERE username='$username' AND password='$pass' ");
+
+$login = mysqli_query($koneksi, "SELECT * FROM tbl_kader WHERE username='$username' AND password='$pass'" );
 	$ketemu = mysqli_num_rows($login);
 	$r = mysqli_fetch_array($login);
 
-	//Apabila username dan password ditemukan
-	if ($ketemu > 0) {
+	if($ketemu > 0 ){
 		session_start();
-
+		
 		$_SESSION['namauser'] = $r['username'];
-		$_SESSION'password'] = $r['password'];
-
+		$_SESSION['password'] = $r['password'];
+        $_SESSION['level']='admin';
+        
 		header('location:adminweb.php');
 	}else{
-		echo "<center>Login Gagal !!! <br>
-		Username atau Password Anda tidak benar.<br>
-		Atau account anda sedang diblokir.<br>
-		</center>";
-		echo "<a href=index.php><b>Ulangi Lagi</b></a>";
-	}
-}
+		echo $pass;
+		exit();
+        echo "<script>alert ('Password salah!'); window.location ='$admin_url';</script>";
+        
+    }
+
+
  ?>
